c10fb228286f05685b5d4aabbc849363443db1c8
ngkhacdai <ngkhacdai@gmail.com>
2023-07-18 18:32:06 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://ngkhacdai:a012675921@assignmentmob402.mbfbglm.mongodb.net/shoplaptop');
 const expressHbs = require('express-handlebars');
